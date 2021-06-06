143cc56d2624d5788b81c99bf1b0cdf32cc78e96
Filipe Gomes <filipe903@gmail.com>
2021-06-05 21:53:19 -0300

    storybook has sucessfully installed

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './templates/App/index';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles } from './styles/global-styles';
+import { theme } from './styles/theme';
+import Home from './templates/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
